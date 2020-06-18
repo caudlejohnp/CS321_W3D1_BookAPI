fce5d11df59c6755cd8b955176c0572c32971b09
caudlejohnp <caudlejohnp@gmail.com>
2020-06-17 19:06:19 -0500

    Implented Author class and new seed data.

--- CS321_W3D1_BookAPI/Data/BookContext.cs
+++ CS321_W3D1_BookAPI/Data/BookContext.cs
@@ -11,6 +11,8 @@ namespace CS321_W3D1_BookAPI.Data
     {
         public DbSet<Book> Books { get; set; }
 
+        public DbSet<Author> Authors { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder myBuilder)
         {
             myBuilder.UseSqlite("Data Source = Books.db");
@@ -21,9 +23,14 @@ namespace CS321_W3D1_BookAPI.Data
             base.OnModelCreating(myModelBuilder);
 
             myModelBuilder.Entity<Book>().HasData(
-                new Book { Id = 1, Title = "The Hobbit", Author = "J.R.R. Tolkein", Category = "Fantasy" },
-                new Book { Id = 2, Title = "The Fellowship of the Ring", Author = "J.R.R. Tolkein", Category = "Fantasy" },
-                new Book { Id = 3, Title = "The Two Towers", Author = "J.R.R. Tolkein", Category = "Fantasy" });
+                new Book { Id = 1, Title = "The Hobbit", AuthorId = 1, Category = "Fantasy" },
+                new Book { Id = 2, Title = "The Fellowship of the Ring", AuthorId = 1, Category = "Fantasy" },
+                new Book { Id = 3, Title = "The Two Towers", AuthorId = 1, Category = "Fantasy" },
+                new Book { Id = 4, Title = "The Broken Mirror", AuthorId = 2, Category = "Fantasy" });
+
+            myModelBuilder.Entity<Author>().HasData(
+                new Author { Id = 1, FirstName = "J.R.R.", LastName = "Tolkein", Birthday = new DateTime(1892, 1, 3) },
+                new Author { Id = 2, FirstName = "Brent", LastName = "Weeks", Birthday = new DateTime(1977, 3, 7) });
         }
 
     }
--- CS321_W3D1_BookAPI/Models/Author.cs
+++ CS321_W3D1_BookAPI/Models/Author.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using CS321_W3D1_BookAPI.Controllers;
 
 namespace CS321_W3D1_BookAPI.Models
 {
-    public class Book
+    public class Author
     {
         public int Id { get; set; }
 
         [Required]
-        public string Title { get; set; }
+        public string FirstName { get; set; }
 
         [Required]
-        public int AuthorId { get; set; }
+        public string LastName { get; set; }
 
         [Required]
-        public string Category { get; set; }
-
-        public Author Author { get; set; }
+        public DateTime Birthday { get; set; }
+        public List<Book> Books { get; set; }
     }
 }
